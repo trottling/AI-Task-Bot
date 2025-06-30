e233733ba9525d3596f112494c559f8dab409b5d
trottling <108357478+trottling@users.noreply.github.com>
2025-06-30 14:15:12 +0300

    Обновлена интеграция с OpenAI

--- config/config.py
+++ config/config.py
@@ -9,7 +9,6 @@ AI_API_KEY = env.str("AI_API_KEY")
 AI_API_MODEL = env.str("AI_API_MODEL")
 AI_API_URL = env.str("AI_API_URL") # ChatGPT, deepseek or local model
 AI_PROXY_URL = env.str("AI_PROXY_URL")
-AI_MODE= env.str("AI_MODE")
 
 with open("./config/schema.json", mode="r", encoding="utf-8", errors="ignore") as f:
     AI_SCHEMA = f.read()
@@ -17,5 +16,5 @@ with open("./config/schema.json", mode="r", encoding="utf-8", errors="ignore") a
 with open("./config/system_prompt.txt", mode="r", encoding="utf-8", errors="ignore") as f:
     AI_SYSTEM_PROMPT = f.read()
 
-with open("./config/system_prompt.txt", mode="r", encoding="utf-8", errors="ignore") as f:
+with open("./config/user_prompt.txt", mode="r", encoding="utf-8", errors="ignore") as f:
     AI_USER_PROMPT = f.read()
